d266539b2c0f92e96d37fa5b5339f78a1839278e
DhaanuI <dhaanu97@gmail.com>
2023-02-17 11:30:13 +0530

    01

--- index.js
+++ index.js
@@ -71,15 +71,15 @@ navLink.addEventListener('click', () => {
 
 
 //navbar fixation 
-const navHeight = navBar.getBoundingClientRect().height
-window.addEventListener('scroll', () => {
-    const scrollHeight = window.pageYOffset
-    if (scrollHeight > navHeight) {
-        navBar.classList.add('fix-nav')
-    } else {
-        navBar.classList.remove('fix-nav')
-    }
-})
+// const navHeight = navBar.getBoundingClientRect().height
+// window.addEventListener('scroll', () => {
+//     const scrollHeight = window.pageYOffset
+//     if (scrollHeight > navHeight) {
+//         navBar.classList.add('fix-nav')
+//     } else {
+//         navBar.classList.remove('fix-nav')
+//     }
+// })
 
 //GlideJs
 
